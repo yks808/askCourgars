22af9107b1c6801322e36633ed3a7a7012de4cfc
Yukina Soga <yksakb48@gmail.com>
2023-03-07 20:31:46 -0700

    added script to connect log in html and js

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function login() {
     const nameEl = document.querySelector("#name");
     localStorage.setItem("userName", nameEl.value);
-    window.location.href = "findquestions.html";
+    window.location.href = "askquestions.html";
 }
