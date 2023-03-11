53e374ee7b348109feeb3266371bd538232241e1
Yukina Soga <yksakb48@gmail.com>
2023-03-10 19:34:25 -0700

    changed some of the function name

--- askquestions.js
+++ askquestions.js
@@ -1,14 +1,15 @@
 
-class Game {
+class Name {
 
     constructor() {
         const playerNameEl = document.querySelector('.player-name');
         playerNameEl.textContent = this.getPlayerName();
     }
+
     getPlayerName() {
         return localStorage.getItem('userName') ?? 'Please log in';
     }
 
 }
 
-const game = new Game();
+const name = new Name();
