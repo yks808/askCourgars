a9be4adbf12a207f6f0f4e4e1735a57cc7ad2a9e
Yukina Soga <yksakb48@gmail.com>
2023-03-22 11:37:23 -0600

    added gitignore

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+const apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
