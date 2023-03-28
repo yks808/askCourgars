33896e9c16b7624fdffacc76010fdbd2058f014c
Yukina Soga <yksakb48@gmail.com>
2023-03-27 21:03:29 -0600

    added database.js and added userfunction

--- database.js
+++ database.js
@@ -15,3 +15,29 @@ const url = `mongodb+srv://${userName}:${password}@${hostname}`;
 const client = new MongoClient(url);
 const userCollection = client.db('askcougars').collection('user');
 
+function getUser(email) {
+    return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = {
+        email: email,
+        password: passwordHash,
+        token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+
+    return user;
+}
+
+module.exports = {
+    getUser,
+    getUserByToken,
+    createUser,
+};
