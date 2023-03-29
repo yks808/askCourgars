29980a0a7d9f2e0d00deb66e34ff3bdb5251f0cf
Yukina Soga <yksakb48@gmail.com>
2023-03-29 11:54:14 -0600

    made it back to original after imprementing service

--- index.js
+++ index.js
@@ -1,99 +1,30 @@
-const cookieParser = require('cookie-parser');
-const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
-const DB = require('./database.js');
 
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
+// The service port. In production the application is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
+// JSON body parsing using built-in middleware
 app.use(express.json());
 
-app.use(cookieParser());
-
+// Serve up the application's static content
 app.use(express.static('public'));
 
-const apiRouter = express.Router();
+// Router for service endpoints
+var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-// Create authtoken for a user
-apiRouter.post('/auth/create', async (req, res) => {
-    if (await DB.getUser(req.body.email)) {
-        res.status(409).send({ msg: 'Error: user already existed' });
-    } else {
-        const user = await DB.createUser(req.body.email, req.body.password);
-
-        setAuthCookie(res, user.token);
-
-        res.send({
-            id: user._id,
-        });
-    }
+// GetScores
+apiRouter.get('/scores', (_req, res) => {
+    res.send(scores);
 });
 
-// Get authtoken for the provided credentials
-apiRouter.post('/auth/login', async (req, res) => {
-    const user = await DB.getUser(req.body.email);
-    if (user) {
-        if (await bcrypt.compare(req.body.password, user.password)) {
-            setAuthCookie(res, user.token);
-            res.send({ id: user._id });
-            return;
-        }
-    }
-    res.status(401).send({ msg: 'Error: unauthorized' });
-});
-
-// DeleteAuth token if stored in cookie
-apiRouter.delete('/auth/logout', (_req, res) => {
-    res.clearCookie(authCookieName);
-    res.status(204).end();
-});
-
-// GetUser returns information about a user
-apiRouter.get('/user/:email', async (req, res) => {
-    const user = await DB.getUser(req.params.email);
-    if (user) {
-        const token = req?.cookies.token;
-        res.send({ email: user.email, authenticated: token === user.token });
-        return;
-    }
-    res.status(404).send({ msg: 'Error: unknown' });
-});
-
-// secureApiRouter verifies credentials for endpoints
-var secureApiRouter = express.Router();
-apiRouter.use(secureApiRouter);
-
-secureApiRouter.use(async (req, res, next) => {
-    authToken = req.cookies[authCookieName];
-    const user = await DB.getUserByToken(authToken);
-    if (user) {
-        next();
-    } else {
-        res.status(401).send({ msg: 'Error: unauthorized' });
-    }
-});
-
-// Default error handler
-app.use(function (err, req, res, next) {
-    res.status(500).send({ type: err.name, message: err.message });
-});
 
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
     res.sendFile('index.html', { root: 'public' });
 });
 
-// setAuthCookie in the HTTP response
-function setAuthCookie(res, authToken) {
-    res.cookie(authCookieName, authToken, {
-        secure: true,
-        httpOnly: true,
-        sameSite: 'strict',
-    });
-}
-
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
-
