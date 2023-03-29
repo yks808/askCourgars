d7c15b2f75a8b0c266122f65ecd585f3e1acaaf9
Yukina Soga <yksakb48@gmail.com>
2023-03-29 16:31:21 -0600

    added quote third part backend

--- public/about.js
+++ public/about.js
@@ -1,2 +1,22 @@
+function displayQuote(data) {
+    fetch('https://api.quotable.io/random')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
 
+            const containerEl = document.querySelector('#quote');
 
+            const quoteEl = document.createElement('p');
+            quoteEl.classList.add('quote');
+            const authorEl = document.createElement('p');
+            authorEl.classList.add('author');
+
+            quoteEl.textContent = data.content;
+            authorEl.textContent = data.author;
+
+            containerEl.appendChild(quoteEl);
+            containerEl.appendChild(authorEl);
+        });
+}
+
+displayQuote();
