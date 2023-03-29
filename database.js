56816962c3138106215b8eeb5c72146864cbd4a2
Yukina Soga <yksakb48@gmail.com>
2023-03-29 17:36:17 -0600

    added database.js

---
+++ database.js
@@ -0,0 +1,44 @@
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+
+const userName = process.env.MONGOUSER;
+const password = process.env.MONGOPASSWORD;
+const hostname = process.env.MONGOHOSTNAME;
+
+if (!userName) {
+    throw Error('Database not configured. Set environment variables');
+}
+
+const url = `mongodb+srv://${userName}:${password}@${hostname}`;
+
+const client = new MongoClient(url);
+const userCollection = client.db('askcougars').collection('user');
+
+function getUser(email) {
+    return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = {
+        email: email,
+        password: passwordHash,
+        token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+
+    return user;
+}
+
+
+module.exports = {
+    getUser,
+    getUserByToken,
+    createUser,
+};
